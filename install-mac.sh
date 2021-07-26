d13cd588b78e918be94fed1a9b0dffb84699be6e
Gal Gofrit <gal.gofrit@gmail.com>
2021-07-26 17:24:42 +0300

    Adding DDCCTL usage in to_win.py - so now running the script should suffice.

---
+++ install-mac.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+brew install ddcctl
+pip3 install -r ./requirements.txt
--- to_win.py
+++ to_win.py
@@ -1,7 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+# Script used to change display settings from macOS->Windows (Samsung G7 HDMI->displayPort), and trigger a KVM switch.
+
+import subprocess
 import serial
 import serial.tools.list_ports
+
+DDCCTL_UTIL = '/usr/local/bin/ddcctl'
+MONITOR_ID = 1 # Assume Samsung G7 is first external display connected
+MONITOR_DISPLAYPORT_ID = 9
 
 def get_serial_port():
     com_ports = serial.tools.list_ports.comports()
@@ -11,7 +18,7 @@ def get_serial_port():
             return port[0]
 
 
-def main():
+def trigger_kvm_switch():
     serial_port_path = get_serial_port()
     if not serial_port_path:
         print('Could not get serial port.')
@@ -21,6 +28,13 @@ def main():
     arduino.write(1)
     arduino.close()
 
+def switch_monitor():
+    subprocess.call([DDCCTL_UTIL, '-d', str(MONITOR_ID), '-i', str(MONITOR_DISPLAYPORT_ID)]) # Assume 
+
+
+def main():
+    switch_monitor()
+    trigger_kvm_switch()
 
 if __name__=='__main__':
     exit(main())
